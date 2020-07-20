49c4426ea413eff1fe139a851381207447b1090c
Dylan Stringer <riggnroll@Dylan-Stringers-MacBook-Pro.local>
2020-07-20 12:45:09 +0200

    fixed clicking problems

--- resources/js/game.js
+++ resources/js/game.js
@@ -15,9 +15,10 @@ const winningCombos = [
 
 /*------ app's state ------*/
 
-let board;
+
+let win = false;
 let turn = 'X';
-let win;
+let moveCount = 0;
 let xScoreDisplay = document.getElementById('xScore');
 let oScoreDisplay = document.getElementById('oScore');
 let xScore = 0;
@@ -28,15 +29,32 @@ let winner;
 
 /*------ cached element references ------*/
 
+  // this constant selects all of the squares in the game and creates an array out of them.
 const squares = Array.from(document.querySelectorAll('#board div'));
 
 /*------ event listeners ------*/
-document.getElementById('board').addEventListener('click', handleTurn)
 const messages = document.querySelector('h2');
 document.getElementById('reset-button').addEventListener('click', init);
 
 
 /*------ functions ------*/
+
+    /*this initializes the game. it attaches an event listener to each square 
+    and fills them with blank inner text. it sets the win variable to false and 
+    renders the messaging at the top. */
+
+function init() {
+  squares.forEach((q) => {
+    q.innerText = "";
+    q.addEventListener("click", handleTurn);
+  })
+  win = false;
+  moveCount = 0;
+  render();
+};
+init();
+
+  //this function allows the player to select whose turn it is at the beginnning of the game.
 function chooseTurn() {
   if (document.getElementById('x').checked) {
     turn = 'X';
@@ -46,44 +64,57 @@ function chooseTurn() {
   init();
 };
 
+function handleTurn() {
+  let index = squares.findIndex(function(square){
+    return square === event.target;
+  })
+  if (squares[index].innerText === "") {
+    moveCount++;
+    squares[index].innerText = turn;
+    squares[index].removeEventListener("click", handleTurn)
+  }
+  win = getWinner();
+  turn = turn === 'X' ? 'O' : 'X';
+  render();
+}
+
+//this function checks if a win has occurred. 
+function check(winIndex) {
+  if (
+    squares[winIndex[0]].innerText !== "" &&
+    squares[winIndex[0]].innerText === squares[winIndex[1]].innerText &&
+    squares[winIndex[0]].innerText === squares[winIndex[2]].innerText
+  ) {
+    return true;
+  }
+  return false;
+}
+
 function getWinner() {
   let winner = null;
-  winningCombos.forEach(function(combo, index) {
-    if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
-      winner = board[combo[0]];
-      updateScore(winner);
-      document.getElementById('board').removeEventListener('click', handleTurn);
-    }
-  })
-  board.includes('') ? null : document.getElementById('board').removeEventListener('click', handleTurn);
-  return winner ? winner : board.includes('') ? null : 'T';
-}
+  //this takes the winning combinations
+  if (moveCount >= 5){
+    winningCombos.forEach((w) => {
+      if (check(w)) {
+        win = true;
+        winner = turn;
+        removeListener();
+        updateScore(winner);
+      }
+      if (moveCount === 9) winner = 'T';
+    });}
+  //problem here vvvv need to id squares inner html
+  return winner ? winner : squares.every((index) => {[index].innerText !== ''}) ? 'T' : null;
+  }
 
-function handleTurn() {
-  let idx = squares.findIndex(function(square) {
-    return square === event.target;
+const removeListener = () => {
+  squares.forEach((q) => {
+    q.removeEventListener("click", handleTurn);
   });
-  board[idx] = turn;
-  turn = turn === 'X' ? 'O' : 'X';
-  win = getWinner();
-  render();
-}
-
-function init() {
-  board = [
-    '', '', '',
-    '', '', '',
-    '', '', '',
-  ];
-  document.getElementById('board').addEventListener('click', handleTurn)
-  render();
 };
 
+
 function render() {
-  board.forEach(function(mark, index){
-      //this moves the value of the board item into the squares[idx]
-    squares[index].textContent = mark;
-  });
   messages.textContent = win === 'T' ? `That's a tie!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
 };
 
@@ -96,5 +127,3 @@ function updateScore(winner) {
   xScoreDisplay.innerText = xScore;
   oScoreDisplay.innerText = oScore;
 }
-
-init();
